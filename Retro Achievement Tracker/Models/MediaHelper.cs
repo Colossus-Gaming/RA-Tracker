de2a63225d56d8d2efdd7586b7fc899ed7fe6497
lrhodes404 <lrhodes404@gmail.com>
2026-01-31 15:28:59 -0500

    Removed MediaToolkit dependency - replaced MediaHelper.cs with .NET 8 compatible implementation using default video width

--- Retro Achievement Tracker/Models/MediaHelper.cs
+++ Retro Achievement Tracker/Models/MediaHelper.cs
@@ -1,47 +1,49 @@
 ﻿using System;
 using System.Drawing;
-using MediaToolkit;
-using MediaToolkit.Model;
-using System.Globalization;
 using System.IO;
 
 namespace Retro_Achievement_Tracker.Models
 {
     class MediaHelper
     {
+        // Default video width used when actual dimensions cannot be determined
+        private const int DefaultVideoWidth = 1028;
+
         internal static string HexConverter(Color color)
         {
             return "#" + color.R.ToString("X2") + color.G.ToString("X2") + color.B.ToString("X2");
         }
 
+        /// <summary>
+        /// Gets the video width for scaling purposes.
+        /// Returns default width since MediaToolkit is not compatible with .NET 8.
+        /// Custom notification videos will use their scale factor applied to the default width.
+        /// </summary>
+        /// <param name="input">Path to the video file</param>
+        /// <returns>Default video width if file exists, 0 otherwise</returns>
         internal static decimal GetVideoWidth(string input)
         {
             if (File.Exists(input))
             {
-                var inputFile = new MediaFile { Filename = input };
-                using (var engine = new Engine())
-                {
-                    engine.GetMetadata(inputFile);
-                }
-                var frameSize = inputFile.Metadata.VideoData.FrameSize;
-
-                return Convert.ToInt32(frameSize.Substring(0, frameSize.IndexOf("x")));
+                // MediaToolkit was removed due to .NET 8 incompatibility.
+                // Return default width - the scale factor will be applied by the caller.
+                return DefaultVideoWidth;
             }
             return 0;
         }
 
+        /// <summary>
+        /// Gets the video duration in milliseconds.
+        /// Returns 0 since MediaToolkit is not compatible with .NET 8.
+        /// This method is kept for API compatibility but video duration 
+        /// should be configured manually via the UI settings.
+        /// </summary>
+        /// <param name="input">Path to the video file</param>
+        /// <returns>0 (duration should be configured via UI settings)</returns>
         internal static int GetVideoDuration(string input)
         {
-            if (File.Exists(input))
-            {
-                var inputFile = new MediaFile { Filename = input };
-
-                using (var engine = new Engine())
-                {
-                    engine.GetMetadata(inputFile);
-                }
-                return Convert.ToInt32(inputFile.Metadata.Duration.TotalMilliseconds, CultureInfo.CurrentCulture);
-            }
+            // MediaToolkit was removed due to .NET 8 incompatibility.
+            // Video duration should be configured via the application settings.
             return 0;
         }
     }
